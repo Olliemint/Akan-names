237284a024b9d7eb703dead77cc75a843786ecae
Olliemint <furywrld5@gmail.com>
2022-03-06 06:38:31 +0300

    add gender picker

--- Js/script.js
+++ Js/script.js
@@ -2,10 +2,23 @@ let button = document.getElementById("btn");
 let form = document.getElementById("form");
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    // date constructor
 
     let formData = new FormData(e.target);
     let myDate = new Date(formData.get("name"));
     let birthDay = myDate.getDay();
+
+    // gender fuctionality
+    var gender = document.getElementsByName("gender");
+     
+    
+
+    for (var i = 0; i < gender.length; i++) {
+        if (gender[i].checked)
+            var genderValue = gender[i].value;
+            
+    }
+    
     
 
     
